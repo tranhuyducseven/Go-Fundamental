26d452f563e5201405b6bb25ed469b5aabe14f5d
tranhuyducseven <tranhuyducseven@gmail.com>
2022-04-30 14:25:48 +0700

    add html/template and render function

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -29,7 +29,7 @@ func loadPage(title string) (*Page, error) {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1> <div>%s</div>", p.Title, (p.Body))
+	renderTemplate(w, "view", p)
 
 }
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,16 +38,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-
-	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\">%s</textarea><br>"+
-		"<input type=\"submit\" value=\"Save\">"+
-		"</form>",
-		p.Title, p.Title, p.Body)
+	renderTemplate(w, "edit", p)
 
 }
-
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles("layout/" + tmpl + ".html")
+	t.Execute(w, p)
+}
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
